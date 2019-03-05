cc2124476989af96fe75e4458c122914118f7bc5
Carlos Ruiz <cruizperez@users.noreply.github.com>
2019-03-05 06:56:20 -0500

    Create SRA_Download.py
    Added SRA downloader

---
+++ SRA_Download.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+"""------------------------- 0.0 Import Modules -----------------------------"""
+
+import sys, argparse, os, subprocess
+
+"""----------------------------- 1.0 Define Functions -----------------------------"""
+
+def SRA_Downloader(Sra_ID, Output):
+    Prefix = Sra_ID[0:2]
+    Six_chars = Sra_ID[0:5]
+    url = "anonftp@ftp.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra/" + Prefix + "/" + Six_chars + "/" + Sra_ID + "/" + Sra_ID + ".sra"
+    output = "~/scratch/" + Sra_ID + "/" + Sra_ID
+    ssh_key = "/nv/hmicro1/cruizperez3/.aspera/connect/etc/asperaweb_id_dsa.openssh"
+    subprocess.call(['ascp', '-i', ssh_key, '-k', '1', '-T', '-l300', url, Output])
+
+
+def main():
+    parser = argparse.ArgumentParser(description='''Given a Blast output and a FastA file, determines which sequences had good matches and retrieves
+                                                    either those with good matches or those without for further processing'''
+                                    'Global mandatory parameters: [FastA_File] [Blast_File] [Output_File]\n'
+                                    'Optional Database Parameters: See ' + sys.argv[0] + ' -h')
+    parser.add_argument('-s', '--sra', dest='Sra_ID', action='store', required=True, help='SRA Identifier to download')
+    parser.add_argument('-o', '--output', dest='Output_File', action='store', required=True, help='Output file')
+    args = parser.parse_args()
+
+    Sra_ID = args.Sra_ID
+    Output_File = args.Output_File
+
+    SRA_Downloader(Sra_ID, Output_File)
+
+if __name__ == "__main__":
+    main()
