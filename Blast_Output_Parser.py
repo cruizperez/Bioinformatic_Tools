7b47fb1d49e17275ceb254082bcd53234759abcf
Carlos Ruiz <cruizperez3@gatech.edu>
2019-04-29 15:49:10 -0400

    Included additional parameters for alignment length calculation.

--- Blast_Output_Parser.py
+++ Blast_Output_Parser.py
@@ -8,7 +8,7 @@ from random import randrange
 
 """----------------------------- 1.0 Define Functions -----------------------------"""
 ### ------------------------------Match filter--------------------------------------
-def HitConfidence(line, id, bitscore, evalue, Aln_Percent = None, Shorter = None):
+def HitConfidence(line, id, bitscore, evalue, Aln_Percent = None, Shorter = None, Query = None, Subject = None):
     if Aln_Percent == None:
         if float(line[2]) >= float(id) and float(line[11]) >= float(bitscore) and float(line[10]) <= float(evalue):
             High_Quality_Match = True
@@ -20,6 +20,16 @@ def HitConfidence(line, id, bitscore, evalue, Aln_Percent = None, Shorter = None
                 High_Quality_Match = True
             else:
                 High_Quality_Match = False
+        elif Query == True:
+            if (int(line[3])*100/int(line[12])) >= float(Aln_Percent)) and float(line[11]) >= float(bitscore) and float(line[2]) >= float(id) and float(line[10]) <= float(evalue):
+                High_Quality_Match = True
+            else:
+                High_Quality_Match = False
+        elif Subject == True:
+            if (int(line[3])*100/int(line[13])) >= float(Aln_Percent)) and float(line[11]) >= float(bitscore) and float(line[2]) >= float(id) and float(line[10]) <= float(evalue):
+                High_Quality_Match = True
+            else:
+                High_Quality_Match = False
         else:
             if (int(line[3])*100/max(int(line[13]),int(line[12])) >= float(Aln_Percent)) and float(line[11]) >= float(bitscore) and float(line[2]) >= float(id) and float(line[10]) <= float(evalue):
                 High_Quality_Match = True
@@ -28,7 +38,7 @@ def HitConfidence(line, id, bitscore, evalue, Aln_Percent = None, Shorter = None
     return(High_Quality_Match)
 
 ### ----------------------------- Blast Parser ----------------------------------------
-def Blast_Parser(BlastFile, Output, id, bitscore, evalue, Aln_Percent = None, Shorter = None):
+def Blast_Parser(BlastFile, Output, id, bitscore, evalue, Aln_Percent = None, Shorter = None, Query = None, Subject = None):
     Blast_Dict = {}
     print("Reading " + BlastFile + " Blast Output")
     # Check if Blast output has qlen and slen in addition to std output.
@@ -52,7 +62,7 @@ def Blast_Parser(BlastFile, Output, id, bitscore, evalue, Aln_Percent = None, Sh
         with open(BlastFile) as BlastFile_Input:
                 for line in BlastFile_Input:
                     line = line.strip().split("\t")
-                    Good = HitConfidence(line, id, bitscore, evalue, Aln_Percent, Shorter)
+                    Good = HitConfidence(line, id, bitscore, evalue, Aln_Percent, Shorter, Query, Subject)
                     if Good == True:
                         if line[0] not in Blast_Dict:
                             Blast_Dict[line[0]] = line[1:]
@@ -79,7 +89,7 @@ def Blast_Parser(BlastFile, Output, id, bitscore, evalue, Aln_Percent = None, Sh
             if long == True:
                 for line in BlastFile_Input:
                     line = line.strip().split("\t")
-                    Good = HitConfidence(line, id, bitscore, evalue, Aln_Percent, Shorter)
+                    Good = HitConfidence(line, id, bitscore, evalue, Aln_Percent, Shorter, Query, Subject)
                     if Good == True:
                         if line[0] not in Blast_Dict:
                             Blast_Dict[line[0]] = line[1:]
@@ -127,6 +137,8 @@ def main():
         parser.add_argument('--evalue', dest='Evalue', action='store', help='Maximum Evalue for a match to be included. By default 10')
         parser.add_argument('--aln_percent', dest='Aln_Percent', action='store', help='If you have qlen and slen in your output, the minimum alignment the match must span to be included. By default not included')
         parser.add_argument('--shorter', action='store_true', help='Calculates the alignment percentage on the shorter sequence, by default false, i.e. calculated on the longer')
+        parser.add_argument('--query', action='store_true', help='Calculates the alignment percentage on the query sequence, by default false, i.e. calculated on the longer')
+        parser.add_argument('--subject', action='store_true', help='Calculates the alignment percentage on the subject sequence, by default false, i.e. calculated on the longer')
         args = parser.parse_args()
 
         Blast_File = args.Blast_File
@@ -136,10 +148,10 @@ def main():
         Evalue = args.Evalue
         Aln_Percent = args.Aln_Percent
         Shorter = args.shorter
-        print(Shorter)
+        Query = args.query
+        Subject = args.subject
 
-
-        Blast_Parser(Blast_File, Output_File, ID_Perc, Bitscore, Evalue, Aln_Percent, Shorter)
+        Blast_Parser(Blast_File, Output_File, ID_Perc, Bitscore, Evalue, Aln_Percent, Shorter, Query, Subject)
 
 if __name__ == "__main__":
     main()
