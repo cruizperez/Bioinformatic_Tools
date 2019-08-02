ce897fefadb7fedb7aabe8b80535a8b2f6c45e14
Carlos Ruiz <cruizperez@users.noreply.github.com>
2019-08-01 21:05:47 -0400

    Added option to name the index

--- SQL_Database_Builder.py
+++ SQL_Database_Builder.py
@@ -16,7 +16,7 @@ you must provide a list of headers as -h header1,header2,header3...
 ################################################################################
 """---1.0 Define Functions---"""
 
-def Create_Table(Database, Input_Table, Table_Name, Column_Names = None):
+def Create_Table(Database, Input_Table, Table_Name, Column_Names = None, Index_Name):
     import sqlite3
     import csv
 
@@ -43,7 +43,7 @@ def Create_Table(Database, Input_Table, Table_Name, Column_Names = None):
                            ", ".join(["%s text" % column for column in columns]))
     cur.execute(sql)
     # Create index from the first column
-    index = "%s" % ("Seq_ID")
+    index = "%s" % (Index_Name)
     sql = "CREATE INDEX %s on %s (%s)" % ( index, Table_Name, columns[0] )
     cur.execute(sql)
 
@@ -72,21 +72,23 @@ def main():
             description='''This script builds a sqlite database from a tab separated table.\n'''
             '''By default it assumes the first line of the input table has headers, if not\n'''
             '''you must provide a list of headers as -h header1,header2,header3...\n'''
-            '''Usage: ''' + sys.argv[0] + ''' -i [Input Table] -d [Database Name] -t [Table Name] --header [Header List]\n'''
-            '''Global mandatory parameters: -i [Input Table] -d [Database Name] -t [Table Name]\n'''
+            '''Usage: ''' + sys.argv[0] + ''' -i [Input Table] -d [Database Name] -t [Table Name] -n [Index Name] --header [Header List]\n'''
+            '''Global mandatory parameters: -i [Input Table] -d [Database Name] -t [Table Name] -n [Index Name]\n'''
             '''Optional Database Parameters: See ''' + sys.argv[0] + ' -h')
     parser.add_argument('-i', '--input', dest='Input_Table', action='store', required=True, help='Input tab-delimited table to parse, by default assumes headers are present')
     parser.add_argument('-d', '--database', dest='Database', action='store', required=True, help='Database name, can be exisiting or new.')
     parser.add_argument('-t', '--table', dest='Table_name', action='store', required=True, help='Table within db to create, always overwrites if tbale with the same name is present.')
+    parser.add_argument('-n', '--index_name', dest='Index_Name', action='store', required=True, help='Name to index the table, must be unique within the database')
     parser.add_argument('--header', dest='Headers', action='store', nargs='+', required=False, help='Header list if not present in input table, enter as --header header1,header2,...')
     args = parser.parse_args()
 
     Input_Table = args.Input_Table
     Database = args.Database
     Table_name = args.Table_name
+    Index_Name = args.Index_Name
     Headers = args.Headers
 
-    Create_Table(Database, Input_Table, Table_name, Headers)
+    Create_Table(Database, Input_Table, Table_name, Headers, Index_Name)
 
 
 if __name__ == "__main__":
