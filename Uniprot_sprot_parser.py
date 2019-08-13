ed55c2ee3bc00d7164d8d15e09c56cbc0219e752
Carlos Ruiz <cruizperez@users.noreply.github.com>
2019-08-12 22:10:35 -0400

    Added KO header

--- Uniprot_sprot_parser.py
+++ Uniprot_sprot_parser.py
@@ -25,7 +25,7 @@ def Parse_Uniprot(Uniprot_Dat, Output, Header = False):
     Uniprot_Dictionary = defaultdict(list)
     Output = open(Output, 'a')
     if Header == True:
-        Output.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format("ID", "Accession", "Gene_Name", "Organism", "Taxonomy", "Function", "Compartment", "Process"))
+        Output.write("{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\n".format("ID", "Accession", "Gene_Name", "KO Number", "Organism", "Taxonomy", "Function", "Compartment", "Process"))
     with open(Uniprot_Dat) as Uniprot:
         ID = ""
         Accession = ""
