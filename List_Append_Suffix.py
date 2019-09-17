889f9fad5aa56f84e70d8d22f2809cbd44dd6c4f
cruizperez <30295020+cruizperez@users.noreply.github.com>
2019-09-17 12:28:59 -0400

    Added List_Append_Suffix

---
+++ List_Append_Suffix.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+"""
+########################################################################
+# Author:	   Carlos Ruiz
+# Intitution:   Georgia Institute of Technology
+# Version:	  2.0
+# Date:		 August 12 2019
+
+# Description: This script appends a prefix or suffix to a given list.
+########################################################################
+"""
+
+################################################################################
+"""---1.0 Define Functions---"""
+
+
+def Line_Appender(Input_File, Output_File, String, Prefix = False):
+    with open(Input_File) as Input, open(Output_File) as Output:
+        if Prefix == True:
+            for line in Input:
+                line = line.strip()
+                Output.write("{}{}".format(String,line))
+        else:
+            for line in Input:
+                line = line.strip()
+                Output.write("{}{}".format(line,String))
+
+
+################################################################################
+"""---3.0 Main Function---"""
+
+def main():
+    import argparse, sys
+    # Setup parser for arguments.
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+            description='''This script appends a suffix or prefix to a list\n'''
+            '''Usage: ''' + sys.argv[0] + ''' -i [Input File] -o [Output File] -s [String to add] --prefix [True or False]\n'''
+            '''Global mandatory parameters: -i [Input File] -o [Output File] -s [String to add]\n'''
+            '''Optional Database Parameters: See ''' + sys.argv[0] + ' -h')
+    parser.add_argument('-i', '--input', dest='Input_File', action='store', required=True, help='Input file')
+    parser.add_argument('-o', '--output', dest='Output_File', action='store', required=True, help='Output file')
+    parser.add_argument('-s', '--string', dest='String', action='store', required=True, help='String to add')
+    parser.add_argument('--prefix', dest='Prefix', action='store_true', required=False, help='Add as prefix, by default add as suffix')
+    args = parser.parse_args()
+
+    Input_File = args.Input_File
+    Output_File = args.Output_File
+    String = args.String
+    Prefix = args.Prefix
+
+
+    Line_Appender(Input_File, Output_File, String, Prefix)
+
+if __name__ == "__main__":
+    main()
