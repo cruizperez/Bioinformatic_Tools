a62ce4b89c744ac49b4aadfd57c570741cafba7e
cruizperez <30295020+cruizperez@users.noreply.github.com>
2019-09-23 16:35:42 -0400

    Added Viral_Contig_Merger.py

--- Viral_Contig_Merger.py
+++ Viral_Contig_Merger.py
@@ -4,29 +4,25 @@
 ########################################################################
 # Author:	   Carlos Ruiz
 # Intitution:   Georgia Institute of Technology
-# Version:	  2.0
-# Date:		 August 12 2019
+# Version:	  1.0
+# Date:		 Sept 23 2019
 
-# Description: This searches IDs provided in a given database.
-# Its use is mainly to search for RefSeq and Uniprot IDs faster than parsing files.
+# Description: This script parses an MCL clustering output and returns
+# the clustered and unclustered contigs
 ########################################################################
 """
-# ----0.0 Prerequisites-----
-
-
-
 
 ################################################################################
 """---1.0 Define Functions---"""
 
-def MCL_to_List(MCL_Output):
+def MCL_to_List(MCL_Output, Contig_Folder, Output_Prefix):
     """ Parses a MCL output and returns a dictionary with contigs that belong
         to each cluster and writes a FastA file with unclustered contigs. """
     import fileinput
     
     Output_dict = {}
     Unclustered_Files = []
-    with open("out.OV_Genomes-vs-OV_Genomes.ANI.mci.I20") as MCL:
+    with open(MCL_Output) as MCL:
         Cluster = 1
         for line in MCL:
             line = line.strip().split()
@@ -39,9 +35,9 @@ def MCL_to_List(MCL_Output):
             else:
                 for genome in line:
                     Unclustered_Files.append(genome)
-    Unclustered_Files = ["01.Final_OV_Contgs/" + s for s in Unclustered_Files]
+    Unclustered_Files = [Contig_Folder + "/" + s for s in Unclustered_Files]
     print(len(Unclustered_Files))
-    with open("Unclustered2.fasta", 'w') as Unclust:
+    with open("{}_unclustered.fasta".format(Output_Prefix), 'w') as Unclust:
         for element in Unclustered_Files:
             with open(element) as input_file:
                 Unclust.write(input_file.read())
@@ -49,9 +45,68 @@ def MCL_to_List(MCL_Output):
     return Output_dict
     # ------------------------
 
-def Cluster_to_Alignment(MCL_Output, Contig_Folder, Output_Folder):
+def Cluster_to_Alignment(Cluster_Dict, Contig_Folder, Output_Prefix, Extension=".fa"):
     import os
-    import pymummer
+    #from pymummer import coords_file, alignment, nucmer
+    from Bio.SeqIO.FastaIO import SimpleFastaParser
+    #from Bio.Seq import Seq
+    import pathlib
+
+    with open("{}_Cluster_Rep.fasta".format(Output_Prefix), 'w') as Output:
+        for cluster, genomes in Cluster_Dict.items():
+            Sizes = []
+            for genome in genomes:
+                path = pathlib.Path(Contig_Folder) / genome
+                with open(path, 'r') as Fasta:
+                    for title, seq in SimpleFastaParser(Fasta):
+                        Sizes.append((title, len(seq)))
+            Sizes.sort(key=lambda tup: tup[1], reverse=True)
+            Seed_genome = None
+            for index, value in enumerate(Sizes):
+                if index == 0:
+                    genome = value[0] + Extension
+                    Seed_genome = pathlib.Path(Contig_Folder) / genome
+                    with open(Seed_genome) as Fasta_Genome:
+                        for title, seq in SimpleFastaParser(Fasta_Genome):
+                            Output.write(">{}\n{}\n".format(title,seq))
+
+# TODO Try to dereplicate genomes...
+                # else:
+                #     Query_name = value[0] + ".fa"
+                #     Query_genome = pathlib.Path(Contig_Folder) / Query_name
+                #     runner = nucmer.Runner(Seed_genome, Query_genome, "Temp_Alignment_{}.coord".format(number), min_id = 95)
+                #     runner.run()
+                #     number+=1
+                #     file_reader = coords_file.reader("Temp_Alignment_{}.coord".format(number))
+                #     alignments = [coord for coord in file_reader if not coord.is_self_hit()] #Remove self hit
 
 
-Clusters = MCL_to_List("Input")
+################################################################################
+"""---2.0 Main Function---"""
+
+def main():
+    import argparse
+    from sys import argv
+
+    # Setup parser for arguments.
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
+            description='''This script parses a MCL output into clutered and unclustered contigs\n'''
+            '''Usage: ''' + argv[0] + ''' -m [MCL Output] -d [Contig Directory] -p [Output Prefix] -e [Extension of files]\n'''
+            '''Global mandatory parameters: -m [MCL Output] -d [Contig Directory] -p [Output Prefix]\n'''
+            '''Optional Database Parameters: See ''' + argv[0] + ' -h')
+    parser.add_argument('-m', '--mclFile', dest='MCL_File', action='store', required=True, help='Input MCL file.')
+    parser.add_argument('-d', '--directory', dest='Contig_Dir', action='store', required=True, help='Directory where contigs are located (one per file).')
+    parser.add_argument('-p', '--prefix', dest='Output_Prefix', action='store', required=True, help='Prefix of output files.')
+    parser.add_argument('-e', '--extension', dest='Extension', action='store', required=True, help='Extension of contig files')
+    args = parser.parse_args()
+
+    MCL_File = args.MCL_File
+    Contig_Dir = args.Contig_Dir
+    Output_Prefix = args.Output_Prefix
+    Extension = args.Extension
+
+    Clusters = MCL_to_List(MCL_File, Contig_Dir, Output_Prefix)
+    Cluster_to_Alignment(Clusters, Contig_Dir, Output_Prefix, Extension)
+
+if __name__ == "__main__":
+    main()
