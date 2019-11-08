0230cb9ffb85ed8ca8556cafe61b738b87e02adb
cruizperez <30295020+cruizperez@users.noreply.github.com>
2019-11-08 18:09:10 -0500

    Updated Pangenome_Matrix_Parser.py

--- Pangenome_Parser_Matrix.py
+++ Pangenome_Parser_Matrix.py
@@ -5,8 +5,8 @@
 # Author:      Carlos Ruiz, cruizperez3@gatech.edu
 # Intitution:   Georgia Institute of Technology
 # https://github.com/cruizperez/
-# Version:    1.0
-# Date:      04 May 2019
+# Version:    2.0
+# Date:      07 Nov 2019
 
 # Description: Parses a binary matrix representing the Pangenome
  of genomes with Orthologous Clusters (OCs) in the rows and genomes
@@ -23,14 +23,15 @@ import numpy as np
 ################################################################################
 """---2.0 Define Functions---"""
 
-def Pangenome(Matrix, Core = 80, Flexible = 50, Permutations = 100):
+def Pangenome(Matrix, Core = 80, Flexible = 50, Permutations = 100, Plot=False):
     # Read input matrix into a dataframe
     Pangenome_Matrix = pd.read_csv(Matrix, sep="\t")
     Num_Genomes = Pangenome_Matrix.shape[1]
     Record_count = 0
     PG_Dictionary = {}
+    Statistics_Dictionary = {}
     # Iterate through permutations.
-    for boot in range(1, Num_Genomes+1):
+    for boot in range(1, Permutations+1):
         # Sample from 1 to number of genomes provided.
         for step in range(1, Num_Genomes):
             # Randomly subsample the original matrix.
@@ -51,6 +52,7 @@ def Pangenome(Matrix, Core = 80, Flexible = 50, Permutations = 100):
             lista = [step,Num_OCs_PG,Num_OCs_FG,Num_OCs_CG]
             PG_Dictionary[Record_count] = lista
             Record_count += 1
+            # Add values 
 
     # Change column names to reflec the percentages requested.
     Num_OCs_Pangenome = 'Num_OCs_Pangenome'
@@ -60,112 +62,141 @@ def Pangenome(Matrix, Core = 80, Flexible = 50, Permutations = 100):
     Pangenome_Table = pd.DataFrame.from_dict(PG_Dictionary, orient = 'index',columns=['Num_Genomes', Num_OCs_Pangenome, Num_OCs_Flexible, Num_OCs_Core])
     Pangenome_Table = Pangenome_Table.sort_values('Num_Genomes')
 
+    if Plot == False:
+        import lmfit
+        from lmfit.models import ExponentialModel, PowerLawModel, ExpressionModel
+
+        # Model the Pangenome using a Powerlaw function  Ps = κn^γ
+        print('\nModelling the Pangenome using a PowerLaw function K*N^\u03B3')
+        PowerLawModel = PowerLawModel()
+        PowerLaw_Parameters_Guess = PowerLawModel.guess(Pangenome_Table.iloc[:,1], x=Pangenome_Table['Num_Genomes'])
+        PowerLaw_Fit = PowerLawModel.fit(Pangenome_Table.iloc[:,1], PowerLaw_Parameters_Guess, x=Pangenome_Table['Num_Genomes'])
+        K = float(PowerLaw_Fit.best_values['amplitude'])
+        Gamma = float(PowerLaw_Fit.best_values['exponent'])
+        print("The equation describing the Pangenome is: {} * N ^ {}".format(round(K,3),round(Gamma,3)))
+        if Gamma < 0:
+            print("The Pangenome of these organisms is closed with \u03B3 < 0.")
+            print("See 10.3389/fmicb.2018.00577 for reference.")
+        elif Gamma <= 1:
+            print("The Pangenome of these organisms is open with 0 < \u03B3 < 1.")
+            print("See 10.3389/fmicb.2018.00577 for reference.")
+
+        # ------------------------
+
+        # Model the Core Genome using an Exponential Decay function  Fc = Kc*exp(-N/τc) + Ω
+        print('\nModelling the Core and Flexible Genome using an Exponential Decay function K*exp-(N/\u03C4) + \u03A9')
+        
+        Custom_Exponential = ExpressionModel('A * exp(-x/tau) + omega', independent_vars=['x'])
+        Expression_Parameters = Custom_Exponential.make_params()
+        # add with tuples: (NAME VALUE VARY MIN  MAX  EXPR  BRUTE_STEP)
+        Expression_Parameters.add_many(('A', 5, True, 0, None, None, 0.1),
+                                        ('tau', 5, True, 0, None, None, 0.1),
+                                        ('omega', 5, True, 0, None, None, 0.1))
+        Core_Genome_Fit = Custom_Exponential.fit(Pangenome_Table.iloc[:,3], Expression_Parameters, x=Pangenome_Table['Num_Genomes'])
+        Flexible_Genome_Fit = Custom_Exponential.fit(Pangenome_Table.iloc[:,2], Expression_Parameters, x=Pangenome_Table['Num_Genomes'])
+
+        print("The equation describing the Core and Flexible genomes is: F = K*exp-(N/\u03C4) + \u03A9")
+        print("Where \u03A9 represents the approximate number of genes where the estimate reaches a plateau")
+        print("Core genome {}% \u03A9: {}".format(Core, round(Core_Genome_Fit.best_values['omega'], 2)))
+        print("Flexible genome {}% \u03A9: {}".format(Flexible, round(Flexible_Genome_Fit.best_values['omega'], 2)))
+
     return Pangenome_Table
 
+# ------------------------------------------------
 
-def Pangenome_Plot(Pangenome_Table, Output_Files_Prefix):
+def Pangenome_Plot(Pangenome_Table, Output_Files_Prefix, Core=80, Flexible=50):
     # Import required modules
-    from lmfit import minimize, Parameters, fit_report
-    from lmfit.models import ExponentialModel, PowerLawModel
     import matplotlib.pyplot as plt
+    import lmfit
+    from lmfit.models import ExponentialModel, PowerLawModel, ExpressionModel
 
-
-    # Model the curves, we fit an exponential for the pangenome and a powerlaw for the core and flexible
-
-    # For the exponential function.
-    #Expo_parameters = Parameters()
-    #Expo_parameters.add('amp', value = -1)
-    #Expo_parameters.add('decay', value = 0)
-    #Expo_parameters.add('offset', value = 0)
-
+    # Model the Pangenome using a Powerlaw function  Ps = κn^γ
+    print('\nModelling the Pangenome using a PowerLaw function K*N^\u03B3')
     PowerLawModel = PowerLawModel()
-    new_param = PowerLawModel.guess(Pangenome_Table.iloc[:,1], x=Pangenome_Table['Num_Genomes'])
-    out = PowerLawModel.fit(Pangenome_Table.iloc[:,1], new_param, x=Pangenome_Table['Num_Genomes'])
+    PowerLaw_Parameters_Guess = PowerLawModel.guess(Pangenome_Table.iloc[:,1], x=Pangenome_Table['Num_Genomes'])
+    PowerLaw_Fit = PowerLawModel.fit(Pangenome_Table.iloc[:,1], PowerLaw_Parameters_Guess, x=Pangenome_Table['Num_Genomes'])
+    K = float(PowerLaw_Fit.best_values['amplitude'])
+    Gamma = float(PowerLaw_Fit.best_values['exponent'])
+    print("The equation describing the Pangenome is: {} * N ^ {}".format(round(K,3),round(Gamma,3)))
+    if Gamma < 0:
+        PanLabel = "Closed"
+        print("The Pangenome of these organisms is closed with \u03B3 < 0.")
+        print("See 10.3389/fmicb.2018.00577 for reference.")
+    elif Gamma <= 1:
+        PanLabel = "Open"
+        print("The Pangenome of these organisms is open with 0 < \u03B3 < 1.")
+        print("See 10.3389/fmicb.2018.00577 for reference.")
+    # ------------------------
+    # Model the Core Genome using an Exponential Decay function  Fc = Kc*exp(-N/τc) + Ω
+    print('\nModelling the Core and Flexible Genome using an Exponential Decay function K*exp-(N/\u03C4) + \u03A9')
+    
+    Custom_Exponential = ExpressionModel('A * exp(-x/tau) + omega', independent_vars=['x'])
+    Expression_Parameters = Custom_Exponential.make_params()
+    # add with tuples: (NAME VALUE VARY MIN  MAX  EXPR  BRUTE_STEP)
+    Expression_Parameters.add_many(('A', 5, True, 0, None, None, 0.1),
+                                    ('tau', 5, True, 0, None, None, 0.1),
+                                    ('omega', 5, True, 0, None, None, 0.1))
+    Core_Genome_Fit = Custom_Exponential.fit(Pangenome_Table.iloc[:,3], Expression_Parameters, x=Pangenome_Table['Num_Genomes'])
+    Flexible_Genome_Fit = Custom_Exponential.fit(Pangenome_Table.iloc[:,2], Expression_Parameters, x=Pangenome_Table['Num_Genomes'])
+    print("The equation describing the Core and Flexible genomes is: F = K*exp-(N/\u03C4) + \u03A9")
+    print("Where \u03A9 represents the approximate number of genes where the estimate reaches a plateau")
+    print("Core genome {}% \u03A9: {}".format(Core, round(Core_Genome_Fit.best_values['omega'], 2)))
+    print("Flexible genome {}% \u03A9: {}".format(Flexible, round(Flexible_Genome_Fit.best_values['omega'], 2)))
+    print("See 10.3389/fmicb.2018.00577 for reference.")
+    
+
+    # Get statistics
+    Num_Genomes = max(Pangenome_Table['Num_Genomes']) + 1
+    # Store lists of Mean, Median, Std
+    Pangenome = [[],[],[]]
+    Core_Genome = [[],[],[]]
+    Flexible_Genome = [[],[],[]]
+
+    for genome in range(1,Num_Genomes):
+        Temp_Table = Pangenome_Table[Pangenome_Table.Num_Genomes == genome]
+        Pangenome[0].append(np.mean(Temp_Table.iloc[:,1]))
+        Pangenome[1].append(np.median(Temp_Table.iloc[:,1]))
+        Pangenome[2].append(np.std(Temp_Table.iloc[:,1]))
+        Core_Genome[0].append(np.mean(Temp_Table.iloc[:,3]))
+        Core_Genome[1].append(np.median(Temp_Table.iloc[:,3]))
+        Core_Genome[2].append(np.std(Temp_Table.iloc[:,3]))
+        Flexible_Genome[0].append(np.mean(Temp_Table.iloc[:,2]))
+        Flexible_Genome[1].append(np.median(Temp_Table.iloc[:,2]))
+        Flexible_Genome[2].append(np.std(Temp_Table.iloc[:,2]))
 
 
-    ExponentialModel = ExponentialModel()
-    new_param2 = ExponentialModel.guess(Pangenome_Table.iloc[:,2], x=Pangenome_Table['Num_Genomes'])
-    out2 = ExponentialModel.fit(Pangenome_Table.iloc[:,2], new_param2, x=Pangenome_Table['Num_Genomes'])
+    Figure, Axis = plt.subplots(1,1, figsize=(15,7), constrained_layout=True, dpi=300)
+    # Plot scatters
+    Axis.scatter(Pangenome_Table['Num_Genomes'], Pangenome_Table['Num_OCs_Pangenome'], color="#393373", alpha = 0.2, label="Pangenome", s=70)
+    Axis.scatter(Pangenome_Table['Num_Genomes'], Pangenome_Table.iloc[:,3], alpha = 0.2, color = '#A82324', label="Core Genome {}%".format(Core), s=70)
+    Axis.scatter(Pangenome_Table['Num_Genomes'], Pangenome_Table.iloc[:,2], alpha = 0.2, color = '#688170', label="Flexible Genome {}%".format(Flexible), s=70)
+    # Plot statistics
+    # Axis.scatter(range(1,Num_Genomes), Pangenome[0], color="grey", s=40, marker="s", label="Mean")
+    Axis.plot(range(1,Num_Genomes), np.asarray(Pangenome[0]) - np.asarray(Pangenome[2]), linewidth=1, color="black", ls=(0, (1, 1)), label="Standard Dev.")
+    Axis.plot(range(1,Num_Genomes), np.asarray(Pangenome[0]) + np.asarray(Pangenome[2]), linewidth=1, color="black", ls=(0, (1, 1)))
+    # Axis.scatter(range(1,Num_Genomes), Core_Genome[0], color="grey", s=40, marker="s")
+    Axis.plot(range(1,Num_Genomes), np.asarray(Core_Genome[0]) - np.asarray(Core_Genome[2]), linewidth=1, color="black", ls=(0, (1, 1)))
+    Axis.plot(range(1,Num_Genomes), np.asarray(Core_Genome[0]) + np.asarray(Core_Genome[2]), linewidth=1, color="black", ls=(0, (1, 1)))
+    # Axis.scatter(range(1,Num_Genomes), Flexible_Genome[0], color="grey", s=40, marker="s")
+    Axis.plot(range(1,Num_Genomes), np.asarray(Flexible_Genome[0]) - np.asarray(Flexible_Genome[2]), linewidth=1, color="black", ls=(0, (1, 1)))
+    Axis.plot(range(1,Num_Genomes), np.asarray(Flexible_Genome[0]) + np.asarray(Flexible_Genome[2]), linewidth=1, color="black", ls=(0, (1, 1)))
+    # Plot best fits
+    Axis.plot(Pangenome_Table['Num_Genomes'], PowerLaw_Fit.best_fit, '-', linewidth=2, label="\u03B3: {} ({} Pangenome)".format(round(Gamma,2),PanLabel), color="#BFAA2E")
+    Axis.plot(Pangenome_Table['Num_Genomes'], Core_Genome_Fit.best_fit, '-', linewidth=2, color="#015C1B", label="Core genes - \u03A9: {}".format(round(Core_Genome_Fit.best_values['omega'])))
+    Axis.plot(Pangenome_Table['Num_Genomes'], Flexible_Genome_Fit.best_fit, '-', linewidth=2, color="#824D55", label="Common genes - \u03A9: {}".format(round(Flexible_Genome_Fit.best_values['omega'])))
+    Legend = Axis.legend(fontsize=12)
+    for text in Legend.get_texts():
+        text.set_color("grey")
+    Axis.tick_params(axis='both', which='major', labelsize=10)
+    Axis.set_xlabel("Number of Genomes", fontsize=15)
+    Axis.set_ylabel("Number of Gene Clusters", fontsize=15)
+    Axis.set_xlim(0,Num_Genomes)
+    Axis.set_ylim(min(Pangenome_Table.iloc[:,3])-500,max(Pangenome_Table.iloc[:,1])+500)
+    Figure.suptitle("Core vs Pangenome Collector Curves", color="#802B18", fontsize=35)
+    Figure.savefig(Output_Files_Prefix + ".png")
 
-    #print(out.fit_report(min_correl=0.25))
-    # Define the exponential function
-    #def exponenial_func(x, amp, decay, offset):
-    #    return amp * np.exp(-decay*x) + offset
-    #
-    #print(out.plot)
-    #print(dir(out))
-    ## Define a function to calculate the residual distances for least squares
-    #def get_residuals_exponential(params, x, data):
-    #    amp = params['amp'].value
-    #    decay = params['decay'].value
-    #    offset = params['offset'].value
-    #
-    #    model = exponenial_func(x, amp, decay, offset)
-    #
-    #    return data - model
-    #
-    #Output = minimize(get_residuals_exponential, Expo_parameters, args=(Pangenome_Table['Num_Genomes'], Pangenome_Table['Num_OCs_Pangenome']))
-    #print(fit_report(out))
-    #
-    #xx = np.linspace(Pangenome_Table['Num_Genomes'], 200, 500)
-    plt.figure(figsize=(10, 10), dpi=300, facecolor='w', edgecolor='k')
-    plt.scatter(Pangenome_Table['Num_Genomes'], Pangenome_Table['Num_OCs_Pangenome'], alpha = 0.08)
-    plt.scatter(Pangenome_Table['Num_Genomes'], Pangenome_Table.iloc[:,2], alpha = 0.08, color = 'r')
-    plt.scatter(Pangenome_Table['Num_Genomes'], Pangenome_Table.iloc[:,3], alpha = 0.08, color = 'g')
-    plt.plot(Pangenome_Table['Num_Genomes'], out.best_fit, 'r-')
-    plt.plot(Pangenome_Table['Num_Genomes'], out2.best_fit, 'r-')
-    plt.savefig(Output_Files_Prefix + '.pdf', dpi=300)
-#plt.plot(Pangenome_Table['Num_Genomes'],yy, "k-")
+# ----------------------------------------------
 
-#%%
-# Create the parameters of the function.
-#from sklearn.metrics import mean_squared_error
-#
-#def exponenial_func(x, a, b, c):
-#    return a*np.exp(-b*x)+c
-#
-#expon = exponenial_func
-#
-#def func_powerlaw(x, m, c, c0):
-#    return c0 + x**m * c
-#
-#target_func = func_powerlaw
-#
-#X = table[0]
-#Y = np.array(table[1])
-#print(np.shape(Y))
-#popt, pcov = curve_fit(target_func, X, table[1], maxfev=2000, p0 = np.asarray([0,10,0]))
-#xx = np.linspace(table[0].min(), 200, 100)
-#yy = func_powerlaw(X, *popt)
-#print("YEAH")
-#print(np.shape(yy))
-#print(mean_squared_error(Y, yy))
-#
-#popt2, pcov2 = curve_fit(expon, X, Y, maxfev=2000, p0 = np.asarray([0,10,0]))
-#xx2 = np.linspace(table[0].min(), table[0].max(), 100)
-#yy2 = exponenial_func(X, *popt2)
-#print(mean_squared_error(table[1], yy2))
-#
-##popt2, pcov2 = curve_fit(expon, table[0], table[2], p0 = np.asarray([1,10,0]), maxfev=5000)
-##xx2 = np.linspace(table[0].min(), 150, 100)
-##yy2 = exponenial_func(xx2, *popt2)
-##
-##popt3, pcov3 = curve_fit(target_func, table[0], table[3], p0 = np.asarray([1,10,0]), maxfev=5000)
-##xx3 = np.linspace(table[0].min(), 150, 100)
-##yy3 = func_powerlaw(xx3, *popt3)
-#
-##Model = np.polyfit(table[0], table[1], 2)
-##plt.subplot(3, 1, 1)
-#plt.scatter(table[0], table[1], alpha = 0.01)
-##plt.subplot(3, 1, 2)
-#plt.scatter(table[0], table[2], color = 'r', alpha = 0.01)
-##plt.subplot(3, 1, 3)
-#plt.scatter(table[0], table[3], color = 'y', alpha = 0.01)
-##plt.show()
-##plt.plot(X,yy, "k-")
-##plt.plot(xx2,yy2, "b-")
-##plt.plot(xx3,yy3, "r-")
-#plt.savefig("Pangenome.png")
 
 """---3.0 Main Function---"""
 
@@ -196,11 +227,11 @@ def main():
     Permutations = args.Permutations
 
     # Run matrix Parser and save matrix
-    Pangenome_Table = Pangenome(Input_Matrix, Core, Flexible, Permutations)
+    Pangenome_Table = Pangenome(Input_Matrix, Core, Flexible, Permutations, Build_Plot)
     Pangenome_Table.to_csv(Output_Files_Prefix + '.tsv', sep="\t", index=False)
 
     if Build_Plot == True:
-        Pangenome_Plot(Pangenome_Table, Output_Files_Prefix)
+        Pangenome_Plot(Pangenome_Table, Output_Files_Prefix, Core, Flexible)
 
 
 
