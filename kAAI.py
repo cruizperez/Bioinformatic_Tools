25f34059b7e5214e69b45d8adfc265cf1c3a25dc
cruizperez <30295020+cruizperez@users.noreply.github.com>
2019-11-25 00:14:11 -0500

    Added multiprocess support

--- kAAI.py
+++ kAAI.py
@@ -36,9 +36,8 @@ def run_hmmsearch(InputFile):
     Prefix = Path(FilePath.stem)
     Folder = FilePath.parent
     Output = Folder / Prefix.with_suffix('.hmm')
-    print(Output)
     Temp_Output = Folder / Prefix.with_suffix('.temp')
-    subprocess.call(["hmmsearch", "--tblout", str(Output), "-o", str(Temp_Output), "--cut_ga", "--cpu", "1", "/mnt/d/Carlos/Desktop/Fast_AAI/02.Kmer_Approach/00.HMM_Models/genes.hmm", str(FilePath)])
+    subprocess.call(["hmmsearch", "--tblout", str(Output), "-o", str(Temp_Output), "--cut_ga", "--cpu", "1", "/mnt/c/Users/Cruiz/OneDrive/Escritorio/kAAI/Multiprocess_Test/00.HMM_Models/genes.hmm", str(FilePath)])
     Temp_Output.unlink()
     return Output
 
@@ -62,6 +61,14 @@ def read_kmers_from_file(filename, positive_hits, ksize):
                 all_kmers += kmers
     return all_kmers
 
+def kAAI_Parser(ID, Kmer_Dictionary, OutFile):
+    with open(OutFile, 'a') as OutFile:
+        # for ID in ID_List:
+        for key2, value2 in Kmer_Dictionary.items():
+            intersection = len(set(Kmer_Dictionary[ID]).intersection(set(value2)))
+            shorter = min(len(list(set(Kmer_Dictionary[ID]))), len(list(set(value2))))
+            fraction = round(intersection/shorter, 3)
+            OutFile.write("{}\t{}\t{}\t{}\t{}\n".format(ID, key2, intersection, shorter, fraction))
 
 ################################################################################
 """---2.0 Main Function---"""
@@ -72,7 +79,8 @@ def main():
     from sys import exit
     from pathlib import Path
     import subprocess
-    import datetime
+    import multiprocessing
+    from functools import partial
 
     # Setup parser for arguments.
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
@@ -85,11 +93,13 @@ def main():
     parser.add_argument('-g', '--genomes', dest='Genome_List', action='store', nargs='+', required=False, help='List of input genomes')
     parser.add_argument('-p', '--proteins', dest='Protein_Files', action='store', nargs='+', required=False, help='List of input protein files')
     parser.add_argument('-o', '--output', dest='Output', action='store', required=True, help='Output File')
+    parser.add_argument('-t', '--threads', dest='Threads', action='store', default=1, type=int, required=False, help='Number of threads to use, by default 1')
     args = parser.parse_args()
 
     Genome_List = args.Genome_List
     Protein_Files = args.Protein_Files
     Output = args.Output
+    Threads = args.Threads
 
     if Genome_List != None:
         print("Predicting proteins...")
@@ -104,10 +114,11 @@ def main():
             HMM_Search_Files.append(HMM_Result)
     elif Protein_Files != None:
         print("Searching HMM models...")
-        HMM_Search_Files = []
-        for Proteins in Protein_Files:
-            HMM_Result = run_hmmsearch(Proteins)
-            HMM_Search_Files.append(HMM_Result)
+        number_processes = Threads
+        pool = multiprocessing.Pool(number_processes)
+        HMM_Search_Files = pool.map(run_hmmsearch, Protein_Files)
+        pool.close()
+        pool.join()
     else:
         exit('No input provided, please provide either genomes "-g" or protein files "-p"')
 
@@ -125,17 +136,17 @@ def main():
                     continue
                 else:
                     Positive_Matches.append(line.strip().split()[0])
-        # HMM_File.unlink()
+        HMM_File.unlink()
         kmers = read_kmers_from_file(SCG_file, Positive_Matches, 8)
         Kmer_Dic[Name] = kmers
     
-    with open(Output, 'w') as OutFile:
-        for key1, value1 in Kmer_Dic.items():
-            for key2, value2 in Kmer_Dic.items():
-                intersection = len(set(value1).intersection(set(value2)))
-                shorter = min(len(list(set(value1))), len(list(set(value2))))
-                fraction = round(intersection/shorter, 3)
-                OutFile.write("{}\t{}\t{}\t{}\t{}\n".format(key1, key2, intersection, shorter, fraction))
+    ID_List = Kmer_Dic.keys()
+    try:
+        pool = pool = multiprocessing.Pool(number_processes)
+        pool.map(partial(kAAI_Parser, Kmer_Dictionary=Kmer_Dic, OutFile=Output), ID_List)
+    finally:
+        pool.close()
+        pool.join()
 
 if __name__ == "__main__":
     main()
