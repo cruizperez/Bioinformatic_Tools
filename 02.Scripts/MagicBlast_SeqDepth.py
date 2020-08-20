16dbe41a11e6ba9c7d6e52a0797b52e548981bd6
cruizperez <cruiz_perez@hotmail.com>
2020-08-20 09:42:05 -0400

    Added sorted option for MagicBlast_SeqDepth

--- 02.Scripts/MagicBlast_SeqDepth.py
+++ 02.Scripts/MagicBlast_SeqDepth.py
@@ -72,6 +72,42 @@ def calculate_seq_depth(magicblast_file, genome_sizes):
     
     return  genome_seq_depth
 
+def calculate_seq_depth_sorted(magicblast_file, genome_sizes, output_table):
+    """
+    Calculates the base-by-base sequencing depth from a sorted file for lower memory consumption.
+    
+    Arguments:
+        magicblast_file {filepath} -- File with MagicBlast tabular output.
+        genome_sizes {dictionary} -- Lengths per sequence
+        output_table {filepath} -- Output table file
+    """
+    current_contig = None
+    current_bases = None
+
+    with open(output_table, 'w') as output, open(magicblast_file, 'r') as magicblast:
+        output.write("Sequence\tPosition\tDepth")
+        for line in magicblast:
+            if line.startswith("#"):
+                continue
+            else:
+                line = line.strip().split()
+                sequence = line[1]
+                seq_start = min(int(line[8]), int(line[9]))
+                seq_end = max(int(line[8]), int(line[9]))
+                if current_contig == None:
+                    current_contig = sequence
+                    current_bases = np.zeros(genome_sizes[sequence])
+                    current_bases[seq_start-1:seq_end-1] += 1
+                elif current_contig == sequence:
+                    current_bases[seq_start-1:seq_end-1] += 1
+                elif current_contig != sequence:
+                    for base, coverage in np.ndenumerate(current_bases):
+                        output.write("{}\t{}\t{}\n".format(current_contig, str(base+1), str(coverage)))
+                    current_contig = sequence
+                    current_bases = np.zeros(genome_sizes[sequence])
+                    current_bases[seq_start-1:seq_end-1] += 1
+
+
 def save_sequencing_depth_table(genome_seq_depth, output_table):
     """
     Saves a dictionary of arrays as a table with
@@ -114,18 +150,23 @@ def main():
                         required=True, help='FastA file of reference sequences')
     parser.add_argument('-o', '--output_table', dest='output_table', action='store', 
                         required=True, help='Output table in the form [Sequence Name]\t[Position]\t[Depth]')
+    parser.add_argument('-s', '--sorted', dest='sorted_input', action='store_true', 
+                        required=False, help='If input is sorted by the second column this will save memory.')
     args = parser.parse_args()
 
     magic_blast = args.magic_blast
     fasta_sequences = args.fasta_sequences
     output_table = args.output_table
+    sorted_input = args.sorted_input
 
     # Calculate Genome Length and Sequencing Depth
     genome_sizes = get_genome_sizes(fasta_sequences)
-    genome_seq_depth = calculate_seq_depth(magic_blast, genome_sizes)
-    
-    # Save output
-    save_sequencing_depth_table(genome_seq_depth, output_table)
+    if sorted_input == True:
+        calculate_seq_depth_sorted(magic_blast, genome_sizes, output_table)
+    else:
+        genome_seq_depth = calculate_seq_depth(magic_blast, genome_sizes)
+        # Save output
+        save_sequencing_depth_table(genome_seq_depth, output_table)
 
 if __name__ == "__main__":
     main()
